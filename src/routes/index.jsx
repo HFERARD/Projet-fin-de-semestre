d947d344de38260c597aa8780596b76639ae5d0a
Nathan Julien <nathan.julien@etu.minesparis.psl.eu>
2026-05-17 17:14:19 +0200

    ajout gitignore, et parge d'accueil

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,40 @@ import { A } from "@solidjs/router";
 export default function Home() {
   return (
     <main>
+      <h1>Bienvenue sur le site du Parking des Mines</h1>
+      <p>Veuillez vous identifier ou créer votre compte client</p>
+      <div id="basicdata">
+        <div class="chip_ac">
+          <p class="info">Première connexion :</p>
+          <div class="encad_btn">
+          <button class="btn_ac"> créer un compte </button>
+          </div>
+        </div>
+        <div class="chip_ac">
+          <p class="info">Déjà client :</p>
+          <div class="formulaire">
+            <div>
+              <label  for="identifiant">Id. : </label>
+              <input  type="text" id="identifiant" name="identifiant" placeholder="à saisir"></input>
+            </div>
+            <div>
+              <label for="Mot de passe">MdP : </label>
+              <input type="password" id="Mdp" name="Mot de passe" placeholder="à saisir"></input>
+            </div>
+          </div>
+          <div class="encad_btn">
+          <button class="btn_ac"> Se connecter </button>
+          </div>
+        </div>
+        
+      </div>
+    </main>
+  );
+}
+
+export function login() {
+  return (
+    <main>
       <h1>Tableau de bord</h1>
       <p>Vue en direct des données du parking</p>
       <div id="basicdata">
