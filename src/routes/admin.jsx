3b3edfa135b85dda5e76104544442c78bbb1bdfc
HFERARD <ferard2006@gmail.com>
2026-05-19 22:34:35 +0200

    Continue implement DB for car list, pb with admin reload

--- src/routes/admin.jsx
+++ src/routes/admin.jsx
@@ -1,4 +1,12 @@
+import { For } from "solid-js";
+import { getCars } from "~/features/spaces/parking_spaces";
+import { createResource } from "solid-js";
+
+
 export default function Admin() {
+    const [cars] = createResource(async () => getCars());
+
+
     return (
         <main>
             <h1>Tableau de bord</h1>
@@ -33,16 +41,14 @@ export default function Admin() {
                     <p class="ligne firstline" title="id_utilisateur">ID de l'utilisateur</p>
                 </div>
                 <hr />
-                <For each={[{ "id": 1, "immatriculation": "AB-123-CD", "heure_arrivee": "2026/05/17 18:20:18", "numero_place": 16, "id_utilisateur": 127 },
-                { "id": 2, "immatriculation": "EF-123-GH", "heure_arrivee": "2026/05/17 22:00:15", "numero_place": 30, "id_utilisateur": 57 }
-                ]}>
+                <For each={cars() || []}>
                     {(voiture) =>
                         <div class="voiture_ligne">
-                            <p class="ligne firstcol">{voiture["id"]}</p>
-                            <p class="ligne">{voiture["immatriculation"]}</p>
-                            <p class="ligne">{voiture["heure_arrivee"]}</p>
-                            <p class="ligne">{voiture["numero_place"]}</p>
-                            <p class="ligne">{voiture["id_utilisateur"]}</p>
+                            <p class="ligne firstcol">Id placeholder</p>
+                            <p class="ligne">{voiture.immat}</p>
+                            <p class="ligne">{voiture.updated_at}</p>
+                            <p class="ligne">{voiture.id}</p>
+                            <p class="ligne">{voiture.car_id}</p>
                         </div>}
                 </For>
             </div>
