f07f6dee796507d75e00811bc065af4278d44593
Nathan Julien <nathan.julien@etu.minesparis.psl.eu>
2026-05-20 22:44:37 +0200

    ajout création compte

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,137 +1,127 @@
-import { CircleOff, UserRoundCheck } from "lucide-solid";
-import { For } from "solid-js";
+import { A } from "@solidjs/router";
 
 export default function Home() {
-    const getIcon = (type) => {
-    switch (type) {
-      case "Avec abonnement":
-        return <UserRoundCheck size={20} color="#01c9d5" stroke-width={3}/>;
-  
-      case "Sans abonnement":
-        return <CircleOff size={20} color="#8b96a1" stroke-width={2}/>;
-    }}
-  
   return (
     <main>
       <h1>Bienvenue sur le site du Parking des Mines</h1>
       <p>Veuillez vous identifier ou créer votre compte client.</p>
       <div id="basicdata">
         <div class="chip_ac">
-          <h2>Notre politique tarifaire</h2>
+          <p class="info">Notre politique tarifaire :</p>
           <div id="liste_tarifs">
             <div class="voiture_ligne">
               <p class="ligne firstline firstcol" title="Durée">Durée</p>
-              <p class="ligne firstline" title="Contrat">Abonnement</p>
+              <p class="ligne firstline" title="Contrat">Contrat</p>
               <p class="ligne firstline" title="Tarif_N">Tarif normal</p>
               <p class="ligne firstline" title="Tarif_VE">Tarif véhicules électriques</p>
               <p class="ligne firstline" title="Tarif_Riv">Tarif riverains</p>
             </div>
             <hr />
             <For each={[
-              {
-                "id": 1,
-                "Durée": "15 minutes",
-                "Contrat": "Sans abonnement",
-                "Tarif_N": "0.80 €",
-                "Tarif_VE": "0.60 €",
-                "Tarif_Riv": "0.40 €"
-              },
-              {
-                "id": 2,
-                "Durée": "1 heure",
-                "Contrat": "Sans abonnement",
-                "Tarif_N": "3.20 €",
-                "Tarif_VE": "2.40 €",
-                "Tarif_Riv": "1.60 €"
-              },
-              {
-                "id": 3,
-                "Durée": "3 heures",
-                "Contrat": "Sans abonnement",
-                "Tarif_N": "9.00 €",
-                "Tarif_VE": "6.80 €",
-                "Tarif_Riv": "4.50 €"
-              },
-              {
-                "id": 4,
-                "Durée": "1 jour",
-                "Contrat": "Sans abonnement",
-                "Tarif_N": "12.50 €",
-                "Tarif_VE": "9.00 €",
-                "Tarif_Riv": "6.20 €"
-              },
-              {
-                "id": 5,
-                "Durée": "1 jour",
-                "Contrat": "Avec abonnement",
-                "Tarif_N": "9.00 €",
-                "Tarif_VE": "6.80 €",
-                "Tarif_Riv": "4.50 €"
-              },
-              {
-                "id": 6,
-                "Durée": "1 semaine",
-                "Contrat": "Sans abonnement",
-                "Tarif_N": "60.00 €",
-                "Tarif_VE": "45.00 €",
-                "Tarif_Riv": "30.00 €"
-              },
-              {
-                "id": 7,
-                "Durée": "1 semaine",
-                "Contrat": "Avec abonnement",
-                "Tarif_N": "42.00 €",
-                "Tarif_VE": "32.00 €",
-                "Tarif_Riv": "21.00 €"
-              },
-              {
-                "id": 8,
-                "Durée": "2 semaines",
-                "Contrat": "Avec abonnement",
-                "Tarif_N": "75.00 €",
-                "Tarif_VE": "56.00 €",
-                "Tarif_Riv": "38.00 €"
-              },
-              {
-                "id": 9,
-                "Durée": "1 mois",
-                "Contrat": "Avec abonnement",
-                "Tarif_N": "110.00 €",
-                "Tarif_VE": "82.00 €",
-                "Tarif_Riv": "55.00 €"
-              },
-              {
-                "id": 10,
-                "Durée": "3 mois",
-                "Contrat": "Avec abonnement",
-                "Tarif_N": "300.00 €",
-                "Tarif_VE": "225.00 €",
-                "Tarif_Riv": "150.00 €"
-              },
-              {
-                "id": 11,
-                "Durée": "6 mois",
-                "Contrat": "Avec abonnement",
-                "Tarif_N": "550.00 €",
-                "Tarif_VE": "412.00 €",
-                "Tarif_Riv": "275.00 €"
-              },
-              {
-                "id": 12,
-                "Durée": "1 an",
-                "Contrat": "Avec abonnement",
-                "Tarif_N": "1000.00 €",
-                "Tarif_VE": "750.00 €",
-                "Tarif_Riv": "500.00 €"
-              }
-            ]}>
+                        {
+                          "id": 1,
+                          "Durée": "15 min",
+                          "Contrat": "Sans abonnement",
+                          "Tarif_N": "0.80 €",
+                          "Tarif_VE": "0.60 €",
+                          "Tarif_Riv": "0.40 €"
+                        },
+                        {
+                          "id": 2,
+                          "Durée": "1h",
+                          "Contrat": "Sans abonnement",
+                          "Tarif_N": "3.20 €",
+                          "Tarif_VE": "2.40 €",
+                          "Tarif_Riv": "1.60 €"
+                        },
+                        {
+                          "id": 3,
+                          "Durée": "3h",
+                          "Contrat": "Sans abonnement",
+                          "Tarif_N": "9.00 €",
+                          "Tarif_VE": "6.80 €",
+                          "Tarif_Riv": "4.50 €"
+                        },
+                        {
+                          "id": 4,
+                          "Durée": "1j",
+                          "Contrat": "Sans abonnement",
+                          "Tarif_N": "12.50 €",
+                          "Tarif_VE": "9.00 €",
+                          "Tarif_Riv": "6.20 €"
+                        },
+                        {
+                          "id": 5,
+                          "Durée": "1j",
+                          "Contrat": "Avec abonnement",
+                          "Tarif_N": "9.00 €",
+                          "Tarif_VE": "6.80 €",
+                          "Tarif_Riv": "4.50 €"
+                        },
+                        {
+                          "id": 6,
+                          "Durée": "1 semaine",
+                          "Contrat": "Sans abonnement",
+                          "Tarif_N": "60.00 €",
+                          "Tarif_VE": "45.00 €",
+                          "Tarif_Riv": "30.00 €"
+                        },
+                        {
+                          "id": 7,
+                          "Durée": "1 semaine",
+                          "Contrat": "Avec abonnement",
+                          "Tarif_N": "42.00 €",
+                          "Tarif_VE": "32.00 €",
+                          "Tarif_Riv": "21.00 €"
+                        },
+                        {
+                          "id": 8,
+                          "Durée": "2 semaines",
+                          "Contrat": "Avec abonnement",
+                          "Tarif_N": "75.00 €",
+                          "Tarif_VE": "56.00 €",
+                          "Tarif_Riv": "38.00 €"
+                        },
+                        {
+                          "id": 9,
+                          "Durée": "1 mois",
+                          "Contrat": "Avec abonnement",
+                          "Tarif_N": "110.00 €",
+                          "Tarif_VE": "82.00 €",
+                          "Tarif_Riv": "55.00 €"
+                        },
+                        {
+                          "id": 10,
+                          "Durée": "3 mois",
+                          "Contrat": "Avec abonnement",
+                          "Tarif_N": "300.00 €",
+                          "Tarif_VE": "225.00 €",
+                          "Tarif_Riv": "150.00 €"
+                        },
+                        {
+                          "id": 11,
+                          "Durée": "6 mois",
+                          "Contrat": "Avec abonnement",
+                          "Tarif_N": "550.00 €",
+                          "Tarif_VE": "412.00 €",
+                          "Tarif_Riv": "275.00 €"
+                        },
+                        {
+                          "id": 12,
+                          "Durée": "1 an",
+                          "Contrat": "Avec abonnement",
+                          "Tarif_N": "1000.00 €",
+                          "Tarif_VE": "750.00 €",
+                          "Tarif_Riv": "500.00 €"
+                        }
+                      ]}>
               {(tarif) =>
-                <div class="voiture_ligne pt-3">
-                  <p class="flex-1">{tarif["Durée"]}</p>
-                  <p class="flex-1">{getIcon(tarif["Contrat"])}</p>
-                  <p class="flex-1 text-white">{tarif["Tarif_N"]}</p>
-                  <p class="flex-1 text-white">{tarif["Tarif_VE"]}</p>
-                  <p class="flex-1 text-white">{tarif["Tarif_Riv"]}</p>
+                <div class="voiture_ligne">
+                  <p class="ligne firstcol">{tarif["Durée"]}</p>
+                  <p class="ligne">{tarif["Contrat"]}</p>
+                  <p class="ligne">{tarif["Tarif_N"]}</p>
+                  <p class="ligne">{tarif["Tarif_VE"]}</p>
+                  <p class="ligne">{tarif["Tarif_Riv"]}</p>
                 </div>}
             </For>
           </div>
@@ -158,10 +148,30 @@ export default function Home() {
           <div class="chip_ac">
             <p class="info">Première connexion :</p>
             <div>
+              <div class="formulaire">
+                <label style="color:black" for="surname">Nom : </label>
+                <input type="text" id="surname" name="surname" placeholder="à saisir"></input>
+              </div>
+              <div class="formulaire">
+                <label style="color:black" for="first_name">Prénom : </label>
+                <input type="text" id="first_name" name="first_name" placeholder="à saisir"></input>
+              </div>
+              <div class="formulaire">
+                <label style="color:black" for="mail">Mail : </label>
+                <input type="text" id="mail" name="mail" placeholder="à saisir"></input>
+              </div>
 
+              <div class="formulaire">
+                <label style="color:black" for="Mot de passe">MdP : </label>
+                <input type="New_password" id="New_Mdp" name="Mot de passe" placeholder="à saisir"></input>
+              </div>
+              <div class="formulaire">
+                <label style="color:black" for="Mot de passe c">MdP :</label>
+                <input type="New_password_c" id="New_Mdp_c" name="Mot de passe c" placeholder=" confirmer votre Mdp"></input>
+              </div>
             </div>
             <div class="encad_btn">
-              <button class="btn_ac">Créer un compte</button>
+              <button class="btn_ac"> créer un compte </button>
             </div>
           </div>
         </div>
@@ -171,7 +181,6 @@ export default function Home() {
   );
 }
 
-/*
 export function Admin() {
   return (
     <main>
@@ -340,4 +349,3 @@ export function Client() {
 
   );
 }
-  */
